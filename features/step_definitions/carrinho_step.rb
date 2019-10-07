f0b413d5e1c3d17a6162bc0e264da45993591549
Taynara Jacon <tjacon@partnerfusion.com>
2019-10-07 14:59:18 -0300

    Improve features

---
+++ features/step_definitions/carrinho_step.rb
@@ -0,0 +1,45 @@
+Dado("que o produto desejado é {string}") do |produto|
+    @produto_nome = produto
+end
+
+Dado("o valor do produto é de {string}") do |valor|
+    @produto_valor = valor
+end
+
+Quando("eu adiciono {int} unidade\\(s)") do |quantidade|
+    quantidade.times do
+        find(".menu-item-info-box", text: @produto_nome.upcase).find('.add-to-cart').click
+    end
+end
+
+Então("deve ser adicionado {int} unidade\\(s) deste item") do |quantidade|
+    cart = find('#cart')
+    expect(cart). to have_text "(#{quantidade}x) #{@produto_nome}"
+end
+
+Então("o valor total deve ser de {string}") do |valor_total|
+    cart = find('#cart')
+    total = cart.find('tr', text:'Total:').find('td')
+    expect(total.text).to eql valor_total
+end
+
+# Lista produtos
+
+Dado("que os produtos desejados são:") do |table|
+    @product_list = table.hashes
+end
+  
+Quando("eu adiciono todos os itens") do
+    @product_list.each do |product|
+        product['quantidade'].to_i.times do
+            find(".menu-item-info-box", text: product["nome"].upcase).find('.add-to-cart').click
+        end
+    end
+end
+  
+Então("vejo todos os itens no carrinho") do
+    cart = find("#cart")
+    @product_list.each do |product|
+        expect(cart).to have_text "(#{product["quantidade"]}x) #{product["nome"]}"
+    end
+end
