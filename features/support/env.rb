b79679d6eab3f8a6d93948cc4b20a59b6b5470b2
Taynara Jacon <tjacon@partnerfusion.com>
2019-10-14 14:32:41 -0300

    Improve code

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,22 @@ require "capybara"
 require "capybara/cucumber"
 require "selenium-webdriver"
 
+
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
+  @browser = ENV["BROWSER"]
+  case @browser
+  when 'chrome'
+    @driver = :selenium_chrome
+  when 'firefox'
+    @driver = :selenium_firefox
+  when 'chrome_headless'
+    @driver = :selenium_chrome_headless
+  when 'firefox_headless'
+    @driver = :selenium_firefox_headless
+  end
+
+  config.default_driver = @driver
   config.app_host = "https://enjoeat-sp4.herokuapp.com"
   config.default_max_wait_time = 10
 end
--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -13,3 +13,10 @@ end
 Before("@green_food") do
   visit "/restaurants/green-food/menu"
 end
+
+After do |scenario|
+  puts scenario.failed?
+  # shot_file = page.save_screenshot("log/screenshot.png")
+  # shot_b64 = Base64.encode64(File.open(shot_file, "rb").read)
+  # embed(shot_b64, "image/png", "Screenshot") #Cucumber anexa o screenshot no report
+end
